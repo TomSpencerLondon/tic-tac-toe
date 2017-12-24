88333c888271bcd8d556e35d78cb186c6b763a62
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-24 15:36:39 +0000

    Add board and moves to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,49 @@
+require_relative 'board'
+
 class Game
 
-  
+  attr_reader :board
+
+  def initialize(board = Board)
+    @board = board.new
+    @marker = :X
+  end
+
+  def get_player_move(move = gets.chomp)
+    raise "Not a valid move" unless valid_move?(move)
+    play(move)
+  end
+
+  private
+
+  def play(move)
+    update_board(move)
+  end
+
+  def valid_move?(move)
+    x, y = convert_move_to_coordinate(move)
+    @board.check_value(x, y) == :empty
+  end
+
+  def update_board(move)
+    x, y = convert_move_to_coordinate(move)
+    @board.set_value(x, y, @marker)
+  end
+
+
+  def convert_move_to_coordinate(move)
+    coordinate_mapping = {
+        "1" => [0, 0],
+        "2" => [1, 0],
+        "3" => [2, 0],
+        "4" => [0, 1],
+        "5" => [1, 1],
+        "6" => [2, 1],
+        "7" => [0, 2],
+        "8" => [1, 2],
+        "9" => [2, 2]
+      }
+      coordinate_mapping[move]
+  end
+
 end
